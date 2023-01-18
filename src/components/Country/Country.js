09669906460006c0757a0125a6b2aa24cfd1c68c
Aaron Ibarra <aaronibarra132@gmail.com>
2023-01-18 14:47:36 -0800

    added additional components

---
+++ src/components/Country/Country.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Country.css';
+
+export default function Country() {
+  return <div className="country">Country</div>;
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Country from '../Country/Country.js';
 
 export default function Main() {
-  return <div>Main</div>;
+  return <Country />;
 }
